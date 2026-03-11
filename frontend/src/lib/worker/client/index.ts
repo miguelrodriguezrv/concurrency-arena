c74b03a051c7c5e1ecf10877c4a74ec511f8a3e0
Miguel Rodriguez <miguel.rodriguez@deus.ai>
2026-03-11 19:34:42 +0100

    chore: general cleanup

--- frontend/src/lib/worker/client/index.ts
+++ frontend/src/lib/worker/client/index.ts
@@ -25,6 +25,7 @@ import type {
 
 type RunnerType = "js" | "go" | "python";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Listener = (payload?: any) => void;
 
 interface ClientOptions {
